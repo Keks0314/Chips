f147c5b763fd05b4d685ee578e1102f1fd2a12f4
John Snow <keks0314@mail.ru>
2019-04-11 16:26:10 +0300

    Наивная реализация поиска слова

--- src/com/Main.java
+++ src/com/Main.java
@@ -7,15 +7,26 @@ import java.util.Arrays;
 public  class Main {
     public static void main(String[] args) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("dictionary.txt"), StandardCharsets.UTF_8))) {
-            char[] selectedWord = ChipsChooser.choose(8);
+            final int countOfWords = 8;
+            char[] selectedWord = ChipsChooser.choose(countOfWords);
+            System.out.println("Отдельно выбранная буква: " + selectedWord[countOfWords - 1]);
             for (char w : selectedWord) {
                 System.out.print(w + "");
             }
             System.out.println();
+            searchCoincidence(selectedWord, reader, countOfWords);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void searchCoincidence(char[] selectedWord, BufferedReader reader, int countOfWords) throws IOException {
+        for (int i = 0; i < countOfWords - 1; ++i) {
+            char[] currentSelectWord = (new String(Arrays.copyOfRange(selectedWord, 0, i + 1)) + selectedWord[countOfWords - 1]).toCharArray();
             String currentWord = null;
             while ((currentWord = reader.readLine()) != null) {
                 char[] charsCurrentWord = currentWord.toCharArray();
-                if (isEqualWords(selectedWord, charsCurrentWord)) {
+                if (isEqualWords(currentSelectWord, charsCurrentWord)) {
                     System.out.print("Слово из словаря: ");
                     for (char c : charsCurrentWord) {
                         System.out.print(c + "");
@@ -23,8 +34,7 @@ public  class Main {
                     System.out.println();
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+            reader = new BufferedReader(new InputStreamReader(new FileInputStream("dictionary.txt"), StandardCharsets.UTF_8));
         }
     }
 
